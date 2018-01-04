6fd7437d10ccda9ff8a1385c84817ea08eef1bfd
Dante <c.dante.federici@gmail.com>
2018-01-04 15:27:09 -0500

    Some fonts

---
+++ fonts.sh
@@ -0,0 +1,18 @@
+##### CONSOLE / CODE
+# A solid font, adobe-source-code-pro
+# pacaur -S adobe-source-code-pro-fonts adobe-source-sans-pro-fonts adobe-source-serif-pro-fonts
+
+##### INTERNATIONAL
+# Chinese, Japanese, Korean, Vietnamese
+# pacaur -S adobe-source-han-sans-otc-fonts
+
+##### EMOJI
+# Also see /etc/fonts/local.conf
+# pacaur -S ttf-freefont ttf-arphic-uming ttf-indic-otf
+
+##### MATH
+# Just the mathematica font
+# pacaur -S font-mathematica
+
+# TeX live/fonts !!!! 1GB !!!!!
+# pacaur -S texlive-core texlive-fontsextra
